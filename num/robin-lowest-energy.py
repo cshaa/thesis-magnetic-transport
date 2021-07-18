824a1b253baa64d66b372a8dada8b00a8ab173db
Michal Grno <m93a.cz@gmail.com>
2021-07-18 03:15:21 +0200

    added introduction, improved code in A.3

--- num/robin-lowest-energy.py
+++ num/robin-lowest-energy.py
@@ -21,7 +21,8 @@ def range_p(step_p: float):
 # after reaching this energy, abort
 min_e = -5
 
-def F(a: float, p: float, e: float) -> float:
+# F(...) = 0 if e is an eigenvalue
+def F(a, p, e) -> float:
     return (
         (a + p)
         * mp.pcfd((e-1)/2, p * mp.sqrt(2))
@@ -30,12 +31,14 @@ def F(a: float, p: float, e: float) -> float:
         * mp.pcfd((e+1)/2, p * mp.sqrt(2))
     )
 
-def is_allowed_energy(a: float, e: float, step_p: float) -> float:
+# checks whether e is is an eigenvalue for any p
+def is_allowed_energy(a, e, step_p) -> float:
     for p in range_p(step_p):
         if F(a, p, e) >= 0:
             return True
     return False
 
+# finds the lowest allowed e
 def find_minimum_energy(
     a: float, start_e: float,
     step_p: float, step_e: float
@@ -48,12 +51,17 @@ def find_minimum_energy(
 
     return e
 
+# returns a tuple (a, e)
 def boundary_minimum_energy_tuple(
     a: float, start_e: float,
     step_p: float, step_e: float
 ) -> tuple[float, float]:
-    return (a, find_minimum_energy(a, start_e, step_p, step_e))
+    return (
+        a,
+        find_minimum_energy(a, start_e, step_p, step_e)
+    )
 
+# prints an array of estimates as CSV
 def print_estimates(
     estimates: list[tuple[float, float]],
     prec: int = 1
@@ -61,6 +69,6 @@ def print_estimates(
     print('boundary, \t energy', flush=True)
     for (a, e) in estimates:
         print(
-            "{0:.1f}, \t {1:.{prec}f}".format(a, e, prec=prec),
+            "{0:.1f}, \t {1:.{p}f}".format(a, e, p=prec),
             flush=True
         )
