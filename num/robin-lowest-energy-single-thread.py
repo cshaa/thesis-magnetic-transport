7be82b031383cbcceaba112b64d860ebd302050d
Michal Grňo <m93a.cz@gmail.com>
2021-07-18 13:27:21 +0200

    upgraded the last of the robin estimates to 4digit precision

--- num/robin-lowest-energy-single-thread.py
+++ num/robin-lowest-energy-single-thread.py
@@ -8,7 +8,7 @@ max_a = Decimal(sys.argv[2]) if len(sys.argv) == 3 else 2
 # print header
 print_estimates([])
 
-e = 1.
+e = -3.5
 for a in float_range(min_a, max_a, 0.1):
     e = find_minimum_energy(a, e, 0.0001, 0.001)
     print(
